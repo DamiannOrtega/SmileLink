5e98e0fd4bc66326d5fb1f857a3957f170f5a8f5
Alan <alangalli100@gmail.com>
2025-12-02 19:58:59 +0000

    funcionando

--- backend_django/smilelink/settings.py
+++ backend_django/smilelink/settings.py
@@ -19,11 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production'
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '10.0.2.2',  # Android Emulator
-    '192.168.1.87',  # Your PC's local IP
-    '0.0.0.0',
+    '*'
 ]
 
 
