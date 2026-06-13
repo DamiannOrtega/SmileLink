0f3a9a1b19d635544f5b4fd25b9a3a7089890418
Miranother <carlosblancoortiz16442@gmail.com>
2026-06-12 18:08:20 -0600

    sifunciona

--- frontend_react/vite.config.ts
+++ frontend_react/vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://10.66.207.165:8000",
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
